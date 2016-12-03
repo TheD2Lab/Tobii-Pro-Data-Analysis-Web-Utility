5391f4153deff487db384fcd6fa3f03934dcbc95
drewmstiles <drewmstiles@gmail.com>
2016-12-02 20:44:46 -0800

    Prototype requests to PHP on localhost

--- upload.php
+++ upload.php
@@ -1,6 +1,6 @@
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
 $uploadOk = 1;
 $fileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -18,11 +18,12 @@ if ($uploadOk == 0) {
 	echo "Sorry, your file was not uploaded";
 }
 else {
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-		echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
-		$response = array();
-		exec("java Hello", $response);
-		print_r($response);
+	if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+	echo 'Hello ' . htmlspecialchars($_REQUEST["name"]) . '?';
+// 		echo "The file ". basename($_FILES["fileToUpload"]["name"]). " has been uploaded.";
+// 		$response = array();
+// 		exec("java Hello", $response);
+// 		print_r($response);
 	}
 	else {
 		echo "Sorry, there was an error uploading the file.";
